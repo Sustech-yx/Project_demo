20ebf205593ad19b51ee812c009fccb63ff932b0
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2021-04-14 16:38:56 +0800

    简化

--- src/xyz/view/ItemComponent.java
+++ src/xyz/view/ItemComponent.java
@@ -5,14 +5,15 @@ import java.awt.*;
 
 public class ItemComponent extends JComponent {
 
-    private Image image;
+    private int num;
     public ItemComponent (int num) {
-        image = ItemUtil.genItem(num);
+        this.num = num;
     }
 
     @Override
     public void paintComponent (Graphics g) {
-
+        super.paintComponent(g);
+        painting(g);
     }
 
     private void painting (Graphics g) {

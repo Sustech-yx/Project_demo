824ef585f36e7e92ebc320dde91605c9e986ca16
Sustech-yx <59305237+Sustech-yx@users.noreply.github.com>
2021-04-17 21:48:18 +0800

    简化

--- src/xyz/view/ItemUtil.java
+++ src/xyz/view/ItemUtil.java
@@ -4,14 +4,10 @@ import java.awt.*;
 
 public class ItemUtil {
     private final static Image mask;
-    private final static Image mine;
-    private final static Image flag;
 
     static {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         mask = toolkit.getImage("src/xyz/view/pic/maskgrid.png");
-        mine = toolkit.getImage("src/xyz/view/pic/gridWithMine.png");
-        flag = toolkit.getImage("src/xyz/view/pic/flagmaskgrid.png");
     }
 
     public static Image genItem (int i) {
